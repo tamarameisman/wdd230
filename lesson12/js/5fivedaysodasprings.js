a250a7c61a892c40dcbf798ed6ba144315a28c17
tamarameisman <tamara@meisman.us>
2021-12-13 23:19:47 -0600

    finishing up

--- lesson12/js/5fivedaysodasprings.js
+++
@@ -1,2 +0,0 @@
-const apiForecastURLSoda = "//api.openweathermap.org/data/2.5/forecast?APPID=22d56379f0c24d5ffba468dd90c28101&id=5607916&units=Imperial";
-getAndShowWeatherForCity(apiForecastURLSoda, 'forecast');
--- lesson12/js/business_data.js
+++ lesson12/js/business_data.js
@@ -1,81 +1,86 @@
-const requestURL = '/lesson12/sanantonio.json';
-
-
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-      console.table(jsonObject); 
-
-      const business = jsonObject;
-
-      for (let i = 0; i < business.length; i++ ) {
-        
-        let card = document.createElement('section');
-        let data =document.createElement("div")
-        let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3');
-
-        let website = document.createElement('a');
-        let motto = document.createElement('p');
-        let phone = document.createElement('p');
-        let image = document.createElement('img');
-
-        h2.textContent = business[i].name ;
-        motto.textContent = business[i].motto;
-        website.textContent = business[i].website;
-        website.setAttribute("href","http://"+business[i].website)
-        phone.textContent = business[i].phone[0].number;
-
-       
-        image.setAttribute('alt', business[i].name);
-        image.setAttribute("src",business[i].logo)
-        data.className="data"
-        data.appendChild(h2);
-        data.appendChild(motto);
-        data.appendChild(website);
-        data.appendChild(phone);
-        card.appendChild(data)
-        card.appendChild(image);
-
-        document.querySelector('div.cards').appendChild(card);
+const requestURL = 'sanantonio.json';
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
     }
-
-    const imagesToLoad = document.querySelectorAll('img[data-src]');
-
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
-};
-
-const imgOptions = {
-    threshold: 1
-};
-//if this is true
-if ('IntersectionObserver' in window) {
-    // is supported so let's go!!!
-    const imgObserver = new IntersectionObserver((items) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                imgObserver.unobserve(item.target);
-            }
-        });
-    }, imgOptions);
-
-    // load image if necessary
-    imagesToLoad.forEach((img) => {
-        imgObserver.observe(img);
-    });
-} else { //just load all images if not supported
-    imagesToLoad.forEach( (img) => {
-        loadImages(img);
-    });
 }
-  }); // temporary checking for valid response and data parsing
 
+loadBusinessData = function(){
+    fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        console.table(jsonObject); 
+        
+        const business = jsonObject;
+        const listgrid = document.getElementById("list-grid").value;
+            
+        const parent = document.getElementById("cards")
+        removeAllChildNodes(parent)
+        for (let i = 0; i < business.length; i++ ) {
+            
+            let card = document.createElement('section');
+            let data =document.createElement("div")
+            let h2 = document.createElement('h2');
+            let h3 = document.createElement('h3');
+            
+            let website = document.createElement('a');
+            let motto = document.createElement('p');
+            let phone = document.createElement('p');
+            let image = document.createElement('img');
+            card.classList.add("view_"+listgrid)
+            h2.textContent = business[i].name ;
+            motto.textContent = business[i].motto;
+            website.textContent = business[i].website;
+            website.setAttribute("href","http://"+business[i].website)
+            phone.textContent = business[i].phone[0].number;
+            
+            
+            image.setAttribute('alt', business[i].name);
+            image.setAttribute("src",business[i].logo)
+            data.className="data"
+            data.appendChild(h2);
+            data.appendChild(motto);
+            data.appendChild(website);
+            data.appendChild(phone);
+            card.appendChild(data)
+            card.appendChild(image);
+            parent.appendChild(card);
+        }
+        const imagesToLoad = document.querySelectorAll('img[data-src]');
+        
+        const loadImages = (image) => {
+            image.setAttribute('src', image.getAttribute('data-src'));
+            image.onload = () => {
+                image.removeAttribute('data-src');
+            };
+        };
+        const imgOptions = {
+            threshold: 1
+        };
+        //if this is true
+        if ('IntersectionObserver' in window) {
+            // is supported so let's go!!!
+            const imgObserver = new IntersectionObserver((items) => {
+                items.forEach((item) => {
+                    if (item.isIntersecting) {
+                        loadImages(item.target);
+                        imgObserver.unobserve(item.target);
+                    }
+                });
+            }, imgOptions);
+            
+            // load image if necessary
+            imagesToLoad.forEach((img) => {
+                imgObserver.observe(img);
+            });
+        } else { //just load all images if not supported
+            imagesToLoad.forEach( (img) => {
+                loadImages(img);
+            });
+        }
+    }); // temporary checking for valid response and data parsing
+}    
 
-
+loadBusinessData();
